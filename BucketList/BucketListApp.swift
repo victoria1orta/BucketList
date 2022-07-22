d1aedd6b8ab79bf0b9e5940546950816ea6087ac
victoria <victoria97molina@yahoo.com>
2022-07-22 15:04:30 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Rolando Orta on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
